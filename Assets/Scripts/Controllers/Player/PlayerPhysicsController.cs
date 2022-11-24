c43da7da58d62ecc0414317932e27ce789276fcf
OnurHG <onurhangumus@gmail.com>
2022-11-24 09:18:41 +0300

    OnScoreIncrease system has added

---
+++ Assets/Scripts/Controllers/Player/PlayerPhysicsController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Signals;
+using Enums;
+using Managers;
+using Data.ValueObject;
+using System;
+
+namespace Controllers
+{
+    public class PlayerPhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+        [SerializeField] private Rigidbody rig;
+        [SerializeField] private PlayerManager manager;
+        #endregion
+        #region Private Variables
+        private PlayerData _data;
+        #endregion
+        #endregion
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _data = manager.GetData();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("TargetCenter"))
+            {
+                ScoreSignals.Instance.onScoreIncrease?.Invoke(ScoreTypeEnums.Score, 1);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+
+        }
+    }
+}
